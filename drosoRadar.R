c3cd99174416bc14cc09851bffbab7295bdc2373
bbarres <benoit.barres@gmail.com>
2018-06-21 19:13:03 +0200

    adding the radarplot code

---
+++ drosoRadar.R
@@ -0,0 +1,56 @@
+###############################################################################
+###############################################################################
+#R code for the Drosophila suzukii pesticide resistance tests paper
+###############################################################################
+###############################################################################
+
+#loading the libraries
+library(fmsb)
+
+#set the working directory
+setwd("~/work/Rfichiers/Githuber/droso_data")
+
+#load the datasets
+dataNa<-read.table("droso_NumAll.txt",header=TRUE,sep="\t")
+dataHS<-read.table("droso_GeneDiv.txt",header=TRUE,sep="\t")
+
+#defining the two colors used for the two populations tested
+colpoly_bord<-c(rgb(0.2,0.1,0.8,0.9),rgb(0.7,0.3,0.3,0.9))
+colpoly_area<-c(rgb(0.2,0.1,0.8,0.4),rgb(0.7,0.3,0.3,0.4))
+
+
+###############################################################################
+#radarplot for the comparison of the Number of alleles between two line
+###############################################################################
+
+radarchart(dataNa,
+           #axis parameters
+           axistype=1,caxislabels=seq(1,5,1),axislabcol="grey50",calcex=1.5,
+           #grid parameters
+           cglty=1,cglwd=2,cglcol="grey70",
+           #labels parameters
+           vlcex=1.5,
+           #polygones parameters
+           pcol=colpoly_bord,pfcol=colpoly_area,plwd=5,plty=1)
+#export .pdf 7*7 inches
+
+
+###############################################################################
+#radarplot for the comparison of the Number of alleles between two line
+###############################################################################
+
+radarchart(dataHS,
+           #axis parameters
+           axistype=1,caxislabels=seq(0,100,25),axislabcol="grey50",calcex=1.5,
+           #grid parameters
+           cglty=1,cglwd=2,cglcol="grey70",
+           #labels parameters
+           vlcex=1.5,
+           #polygones parameters
+           pcol=colpoly_bord,pfcol=colpoly_area,plwd=5,plty=1)
+#export .pdf 7*7 inches
+
+
+###############################################################################
+#END
+###############################################################################
