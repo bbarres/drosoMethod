6734d94028463e1e3474341fbb237c37bd03b283
bbarres <benoit.barres@gmail.com>
2019-05-17 18:01:58 +0200

    trying to figure out how to test for the effect of time of the exposure on the DL50

--- droso_expoeffect.R
+++ droso_expoeffect.R
@@ -25,6 +25,26 @@ EDcomp(expo_mod,c(50,50))
 exporez<-ED(expo_mod,50,interval="delta",reference="control")
 
 
+
+#it seems that EDcomp is not working with LN models ? Therefore we can do
+#the computation with LL instead (the results are quite similar anyway)
+expo_mod<-drm(dead/total~dose,weights=total,
+              data=expodata,curveid=repet,
+              fct=LL.3u(),
+              type="binomial")
+plot(expo_mod,type="confidence")
+plot(expo_mod,type="obs",add=TRUE)
+temp<-EDcomp(expo_mod,c(50,50))
+exporez<-ED(expo_mod,50,interval="delta",reference="control")
+
+plot(as.data.frame(exporez)$Estimate,ylim=c(0,0.3))
+
+
+#performing a logistic regression to analyse both the sex and duration of 
+#exposure effects at the same time
+logmod<-glm(cbind(expodata$alive,expodata$dead)~dose*sex*exposition,
+            family=binomial(link=probit),data=expodata)
+
 ###############################################################################
 #END
 ###############################################################################
