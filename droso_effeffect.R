1e30527969c21b9ed7fb4f08bc85b3121f0e79f9
bbarres <benoit.barres@gmail.com>
2020-04-11 22:27:21 +0200

    correction of a mistake for the figure 5

--- droso_effeffect.R
+++ droso_effeffect.R
@@ -16,14 +16,17 @@ numberdata<-dataDroz[dataDroz$number_comp==1,]
 #because on the 2016-06-23, the behaviour of the flies was abnormal, 
 #this repetition was removed before analysis
 numberdata<-numberdata[numberdata$date!="2016-06-23",]
-
 #first we remove unnecessary levels in the data frame
 numberdata<-drop.levels(numberdata)
+
 #we then create a dataframe for the results
 REZdroz<-data.frame("repet"=as.character(),"ED50"=as.numeric(),
                     "IC_low"=as.numeric(),"IC_up"=as.numeric(),
                     "SE"=as.numeric())
-#and here comes the loop
+
+countEffec<-aggregate(cbind(dead,total)~date+sex+repet,data=numberdata,"sum")
+
+#and here comes the loop for LD50 evaluation
 for (i in 1: length(levels(numberdata$repet))) {
   temp.m1<-drm(dead/total~dose,weights=total,
                data=numberdata[numberdata$repet==levels(numberdata$repet)[i],],
@@ -36,7 +39,7 @@ for (i in 1: length(levels(numberdata$repet))) {
   REZdroz<-rbind(REZdroz,tempx)
 }
 REZdroz<-REZdroz[order(as.character(REZdroz$date)),]
-results<- merge(checkdat,REZdroz,by.x="repet",by.y="date",all=FALSE)
+results<- merge(countEffec,REZdroz,by.x="repet",by.y="date",all=FALSE)
 
 
 ##############################################################################/
@@ -48,10 +51,10 @@ legx<-expression(bold("Mean number of ")*bolditalic("D. suzukii ")*
 op<-par(mar=c(5,5,1,1),mfrow=c(1,2))
 plot(results$ED50[results$sex=="female"]~results$total[results$sex=="female"],
      xlab ="Mean number of D. suzukii per dose",ylab="LD50 (mg/l)",
-     ylim=c(0,100),xlim=c(50,270),bty="n",ann=FALSE,axes=FALSE,
+     ylim=c(0,100),xlim=c(20,270),bty="n",ann=FALSE,axes=FALSE,
      cex=2,pch=21,col=rgb(0,0,0,0.0),bg=rgb(0,0,0,0.0))
-box(lwd=3,lty=1)
-axis(1,at=c(70,105,140,175,210,245),labels=c("10","15","20","25","30","35"),
+axis(1,at=c(35,70,105,140,175,210,245),
+     labels=c("5","10","15","20","25","30","35"),
      cex.axis=1.5,font.axis=2,lwd.ticks=2)
 axis(2,at=c(0,20,40,60,80,100),labels=c("0","20","40","60","80","100"),
      cex.axis=1.5,font.axis=2,lwd.ticks=2,las=1)
@@ -76,14 +79,15 @@ plotCI(results$total[results$sex=="female"],
        #li=results$IC_low[results$sex=="female"],
        add=TRUE,cex=2,pch=21,col=rgb(0,0,0,1),pt.bg=rgb(0.7,0.7,0.7,1),
        gap=0.014)
+box(lwd=3,lty=1)
 
 par(mar=c(5,2,1,4))
 plot(results$ED50[results$sex=="male"]~results$total[results$sex=="male"],
      xlab ="Mean number of D. suzukii per dose",ylab="LD50 (mg/l)",
-     ylim=c(0,100),xlim=c(50,270),bty="n",ann=FALSE,axes=FALSE,
+     ylim=c(0,100),xlim=c(20,270),bty="n",ann=FALSE,axes=FALSE,
      cex=2,pch=24,col=rgb(0,0,0,0.0),bg=rgb(0,0,0,0.0))
-box(lwd=3,lty=1)
-axis(1,at=c(70,105,140,175,210,245),labels=c("10","15","20","25","30","35"),
+axis(1,at=c(35,70,105,140,175,210,245),
+     labels=c("5","10","15","20","25","30","35"),
      cex.axis=1.5,font.axis=2,lwd.ticks=2)
 axis(2,at=c(0,20,40,60,80,100),labels=FALSE,
      cex.axis=1.5,font.axis=2,lwd.ticks=2,las=1)
@@ -108,6 +112,7 @@ plotCI(results$total[results$sex=="male"],
        #li=results$IC_low[results$sex=="male"],
        add=TRUE,cex=2,pch=24,col=rgb(0,0,0,1),pt.bg=rgb(1,1,1,1.0),
        gap=0.011)
+box(lwd=3,lty=1)
 par(op)
 #export to pdf 15*7 inches
 
